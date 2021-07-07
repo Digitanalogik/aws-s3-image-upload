b5ff747a3281e662b6e2898a80d89cef7a007060
Tatu Soininen <taatusti@gmail.com>
2021-07-08 01:23:48 +0300

    Store AWS credentials in a private file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
-import ReactS3, { uploadFile } from 'react-s3';
+import ReactS3 from 'react-s3';
 import './App.css';
-
-const config = {
-  bucketName: 'AWS_S3_BUCKET',
-  dirName: 'DIRECTORY_PATH',
-  region: 'eu-north-1',
-  accessKeyId: 'AWS_ACCESS_KEY_ID',
-  secretAccessKey: 'AWS_SECRET_ACCESS_KEY_CHANGE_REAL_VALUES',
-}
+import { s3_config } from './private/aws.js';
 
 const App = () => {
   const [image, setImage] = useState(null);
 
   const uploadFile = (e) => {
     console.log("Upload!", e.target.files[0]);
-    ReactS3.uploadFile(e.target.files[0], config)
+    ReactS3.uploadFile(e.target.files[0], s3_config)
       .then( (data) => {
         console.log(data);
         setImage(data.location);
@@ -29,7 +22,7 @@ const App = () => {
     <div className="App">
       <h3>AWS S3 Upload</h3>
       <input type="file" onChange={(e) => uploadFile(e)} />
-      {image && <img src={image} className="preview" />}
+      {image && <img src={image} className="preview" alt="Succesful upload" />}
     </div>
   );
 }
